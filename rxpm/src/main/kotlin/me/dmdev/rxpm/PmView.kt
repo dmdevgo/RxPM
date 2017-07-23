46e39fa5c9d9ec53164e3f3c204003d894d5ceca
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2017-07-24 01:29:23 +0300

    Added extensions for binding

--- rxpm/src/main/kotlin/me/dmdev/rxpm/PmView.kt
+++ rxpm/src/main/kotlin/me/dmdev/rxpm/PmView.kt
@@ -1,6 +1,10 @@
 package me.dmdev.rxpm
 
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
 
 /**
  * @author Dmitriy Gorbunov
@@ -11,4 +15,20 @@ interface PmView<out PM : PresentationModel> {
     fun providePresentationModel(): PM
     fun onBindPresentationModel()
     fun onUnbindPresentationModel()
+    fun <T> Observable<T>.bindTo(consumer: Consumer<in T>) {
+        compositeDisposable.add(
+                this
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(consumer)
+        )
+    }
+
+    fun <T> Observable<T>.bindTo(consumer: (T) -> Unit) {
+        compositeDisposable.add(
+                this
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(consumer)
+        )
+    }
+    fun Disposable.untilUnbind() = compositeDisposable.add(this)
 }
