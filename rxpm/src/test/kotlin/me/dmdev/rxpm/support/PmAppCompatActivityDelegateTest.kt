51499df4bf58e0736d5717e20638d3196df9bff2
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2017-07-26 00:14:02 +0300

    Added simple tests for delegates

---
+++ rxpm/src/test/kotlin/me/dmdev/rxpm/support/PmAppCompatActivityDelegateTest.kt
@@ -0,0 +1,87 @@
+package me.dmdev.rxpm.support
+
+import android.app.Activity
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.spy
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.TestObserver
+import me.dmdev.rxpm.PmView
+import me.dmdev.rxpm.PresentationModel
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+/**
+ * @author Dmitriy Gorbunov
+ */
+class PmAppCompatActivityDelegateTest {
+
+    lateinit var pm: PresentationModel
+    lateinit var compositeDisposableMock: CompositeDisposable
+    lateinit var pmViewMock: PmView<PresentationModel>
+    lateinit var activityMock: Activity
+
+    @Before
+    fun initTest() {
+        pm = spy<PresentationModel>()
+        compositeDisposableMock = mock<CompositeDisposable>()
+
+        pmViewMock = mock<PmView<PresentationModel>>()
+        whenever(pmViewMock.compositeDisposable).thenReturn(compositeDisposableMock)
+        whenever(pmViewMock.providePresentationModel()).thenReturn(pm)
+
+        activityMock = mock<Activity>()
+    }
+
+    @Test
+    fun testViewLifeCycle() {
+
+        val delegate = PmAppCompatActivityDelegate(activityMock, pmViewMock)
+
+        delegate.onCreate(null)
+
+        verify(pmViewMock).providePresentationModel()
+        assertEquals(pm, delegate.pm)
+
+        delegate.onStart()
+        verify(pmViewMock).onBindPresentationModel()
+
+        delegate.onResume()
+        delegate.onPause()
+
+        delegate.onStop()
+
+        verify(pmViewMock).onUnbindPresentationModel()
+        verify(compositeDisposableMock).clear()
+
+        whenever(activityMock.isFinishing).thenReturn(true)
+        delegate.onDestroy()
+    }
+
+    @Test
+    fun testPresentationModelLifeCycle() {
+
+        val testObserver = TestObserver<PresentationModel.LifeCycleState>()
+        pm.lifeCycleObservable.subscribe(testObserver)
+
+        val delegate = PmAppCompatActivityDelegate(activityMock, pmViewMock)
+
+        delegate.onCreate(null)
+        delegate.onStart()
+        delegate.onResume()
+        delegate.onPause()
+        delegate.onStop()
+        whenever(activityMock.isFinishing).thenReturn(true)
+        delegate.onDestroy()
+
+        testObserver.assertValues(PresentationModel.LifeCycleState.NULL,
+                                  PresentationModel.LifeCycleState.ON_CREATE,
+                                  PresentationModel.LifeCycleState.ON_BIND,
+                                  PresentationModel.LifeCycleState.ON_UNBIND,
+                                  PresentationModel.LifeCycleState.ON_DESTROY)
+    }
+
+}
