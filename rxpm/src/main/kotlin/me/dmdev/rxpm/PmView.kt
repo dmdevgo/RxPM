4ac79b54bf110f9de8315d22a152ba5257e7f015
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2017-08-10 00:00:56 +0300

    Added bindTo extensions for controls

--- rxpm/src/main/kotlin/me/dmdev/rxpm/PmView.kt
+++ rxpm/src/main/kotlin/me/dmdev/rxpm/PmView.kt
@@ -1,10 +1,18 @@
 package me.dmdev.rxpm
 
+import android.support.design.widget.TextInputLayout
+import android.view.View
+import android.widget.CompoundButton
+import android.widget.EditText
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.functions.Consumer
+import me.dmdev.rxpm.widget.CheckControl
+import me.dmdev.rxpm.widget.ClickControl
+import me.dmdev.rxpm.widget.InputControl
+import me.dmdev.rxpm.widget.bind
 
 /**
  * @author Dmitriy Gorbunov
@@ -62,6 +70,34 @@ interface PmView<PM : PresentationModel> {
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(consumer)
         )
+    }
+
+    /**
+     * Local extension to bind [InputControl] to [EditText], use it ONLY in [onBindPresentationModel]
+     */
+    infix fun InputControl.bindTo(editText: EditText) {
+        compositeUnbind.add(editText.bind(this))
+    }
+
+    /**
+     * Local extension to bind [InputControl] to [TextInputLayout], use it ONLY in [onBindPresentationModel]
+     */
+    infix fun InputControl.bindTo(textInputLayout: TextInputLayout) {
+        compositeUnbind.add(textInputLayout.bind(this))
+    }
+
+    /**
+     * Local extension to bind [CheckControl] to [CompoundButton], use it ONLY in [onBindPresentationModel]
+     */
+    infix fun CheckControl.bindTo(compoundButton: CompoundButton) {
+        compositeUnbind.add(compoundButton.bind(this))
+    }
+
+    /**
+     * Local extension to bind [ClickControl] to [View], use it ONLY in [onBindPresentationModel]
+     */
+    infix fun ClickControl.bindTo(view: View) {
+        compositeUnbind.add(view.bind(this))
     }
 
     /**
