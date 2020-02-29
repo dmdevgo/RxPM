1e73fb0a01db502a6075ac82c0c0777cb98d692a
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2020-02-29 15:59:01 +0300

    Remove unnecessary inlines

--- rxpm/src/main/kotlin/me/dmdev/rxpm/PmExtensions.kt
+++ rxpm/src/main/kotlin/me/dmdev/rxpm/PmExtensions.kt
@@ -1,5 +1,3 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
 package me.dmdev.rxpm
 
 import com.jakewharton.rxrelay2.*
@@ -13,7 +11,7 @@ import me.dmdev.rxpm.util.*
  * Convenience to get this [Relay] as an [Observable].
  * Helps to ensure code readability.
  */
-inline fun <T> Relay<T>.asObservable(): Observable<T> {
+fun <T> Relay<T>.asObservable(): Observable<T> {
     return this.hide()
 }
 
@@ -21,14 +19,14 @@ inline fun <T> Relay<T>.asObservable(): Observable<T> {
  * Convenience to get this [Relay] as an [Consumer].
  * Helps to ensure code readability.
  */
-inline fun <T> Relay<T>.asConsumer(): Consumer<T> {
+fun <T> Relay<T>.asConsumer(): Consumer<T> {
     return this
 }
 
 /**
  * Convenience to bind the [progress][progressConsumer] to the [Single].
  */
-inline fun <T> Single<T>.bindProgress(progressConsumer: Consumer<Boolean>): Single<T> {
+fun <T> Single<T>.bindProgress(progressConsumer: Consumer<Boolean>): Single<T> {
     return this
         .doOnSubscribe { progressConsumer.accept(true) }
         .doFinally { progressConsumer.accept(false) }
@@ -37,7 +35,7 @@ inline fun <T> Single<T>.bindProgress(progressConsumer: Consumer<Boolean>): Sing
 /**
  * Convenience to bind the [progress][progressConsumer] to the [Maybe].
  */
-inline fun <T> Maybe<T>.bindProgress(progressConsumer: Consumer<Boolean>): Maybe<T> {
+fun <T> Maybe<T>.bindProgress(progressConsumer: Consumer<Boolean>): Maybe<T> {
     return this
         .doOnSubscribe { progressConsumer.accept(true) }
         .doFinally { progressConsumer.accept(false) }
@@ -46,7 +44,7 @@ inline fun <T> Maybe<T>.bindProgress(progressConsumer: Consumer<Boolean>): Maybe
 /**
  * Convenience to bind the [progress][progressConsumer] to the [Completable].
  */
-inline fun Completable.bindProgress(progressConsumer: Consumer<Boolean>): Completable {
+fun Completable.bindProgress(progressConsumer: Consumer<Boolean>): Completable {
     return this
         .doOnSubscribe { progressConsumer.accept(true) }
         .doFinally { progressConsumer.accept(false) }
@@ -76,7 +74,7 @@ fun Completable.bindProgress(state: State<Boolean>): Completable {
 /**
  * Convenience to filter out items emitted by the source [Observable] when in progress ([progressState] last value is `true`).
  */
-inline fun <T> Observable<T>.skipWhileInProgress(progressState: Observable<Boolean>): Observable<T> {
+fun <T> Observable<T>.skipWhileInProgress(progressState: Observable<Boolean>): Observable<T> {
     return this
         .withLatestFrom(
             progressState.startWith(false),
--- sample/src/main/kotlin/me/dmdev/rxpm/sample/main/extensions/FragmentManagerExtensions.kt
+++ sample/src/main/kotlin/me/dmdev/rxpm/sample/main/extensions/FragmentManagerExtensions.kt
@@ -1,12 +1,10 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
 package me.dmdev.rxpm.sample.main.extensions
 
 import androidx.fragment.app.*
 import me.dmdev.rxpm.sample.*
 
 
-inline fun FragmentManager.openScreen(
+fun FragmentManager.openScreen(
     fragment: Fragment,
     tag: String = fragment.javaClass.name,
     addToBackStack: Boolean = true
@@ -20,7 +18,7 @@ inline fun FragmentManager.openScreen(
 inline val FragmentManager.currentScreen: Fragment?
     get() = this.findFragmentById(R.id.container)
 
-inline fun FragmentManager.back() {
+fun FragmentManager.back() {
     popBackStackImmediate()
 }
 
@@ -28,13 +26,13 @@ inline fun <reified T> FragmentManager.findScreen(): T? {
     return findFragmentByTag(T::class.java.name) as? T
 }
 
-inline fun FragmentManager.clearBackStack() {
+fun FragmentManager.clearBackStack() {
     for (i in 0..backStackEntryCount) {
         popBackStackImmediate()
     }
 }
 
-inline fun FragmentManager.showDialog(
+fun FragmentManager.showDialog(
     dialog: DialogFragment,
     tag: String = dialog.javaClass.name
 ) {
