9660cc0e05ebfdbdcfc98cda815f19e92233ce6c
Dmitriy Gorbunov <dmitriy.goto@gmail.com>
2021-02-07 20:26:11 +0300

    Update copyrights

--- rxpm/src/main/kotlin/me/dmdev/rxpm/PmExtensions.kt
+++ rxpm/src/main/kotlin/me/dmdev/rxpm/PmExtensions.kt
@@ -1,10 +1,40 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2021 Dmitriy Gorbunov (dmitriy.goto@gmail.com)
+ *                     and Vasili Chyrvon (vasili.chyrvon@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.dmdev.rxpm
 
-import com.jakewharton.rxrelay2.*
-import io.reactivex.*
-import io.reactivex.android.schedulers.*
-import io.reactivex.functions.*
-import me.dmdev.rxpm.util.*
+import com.jakewharton.rxrelay2.Relay
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import io.reactivex.functions.Consumer
+import me.dmdev.rxpm.util.BufferSingleValueWhileIdleOperator
+import me.dmdev.rxpm.util.BufferWhileIdleOperator
 
 
 /**
--- rxpm/src/test/kotlin/me/dmdev/rxpm/util/SchedulersRule.kt
+++ rxpm/src/test/kotlin/me/dmdev/rxpm/util/SchedulersRule.kt
@@ -1,9 +1,35 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2021 Dmitriy Gorbunov (dmitriy.goto@gmail.com)
+ *                     and Vasili Chyrvon (vasili.chyrvon@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.dmdev.rxpm.util
 
-import io.reactivex.android.plugins.*
-import io.reactivex.plugins.*
-import io.reactivex.schedulers.*
-import org.junit.rules.*
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.TestScheduler
+import org.junit.rules.ExternalResource
 
 class SchedulersRule(private val useTestScheduler: Boolean = false) : ExternalResource() {
 
